fbb7a799f4a25aa64186e6e57d79d0f3d0cdb4a0
Clayton Wong <claytonjwong@gmail.com>
2020-09-24 13:20:58 -0700

    Triva app (Fragment + Nav Controller)

--- Trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ Trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,13 +1,12 @@
 package com.example.android.navigation
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.R.id.action_titleFragment_to_gameFragment
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
@@ -32,8 +31,17 @@ class TitleFragment : Fragment() {
             // 3. the most concise way to handle a click event...
 //            Navigation.createNavigateOnClickListener(action_titleFragment_to_gameFragment)
         }
-
+        setHasOptionsMenu(true)
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!, view!!.findNavController())
+            || return super.onOptionsItemSelected(item)
+    }
 }
